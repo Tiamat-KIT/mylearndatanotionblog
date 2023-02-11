73b8a124f752a22703af82070fc8b0e396ffed7f
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2023-02-11 23:55:17 +0000

    padding-fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,26 +9,28 @@ export default async function Home() {
   return (
     <div  className={``}>
       <h1 className="hero text-5xl font-bold pt-10">MyNotion-LearnMemo</h1>
-      <div className={`grid grid-rows-4 grid-flow-col justify-around py-5`}>
+      <div className={`grid grid-rows-4 grid-flow-col justify-around`}>
       {PageData.map((data,i) => {
         return (
-          <div key={i} className={`card card-bordered	border-slate-400 shadow-xl`}>
-            <div className={`card-body rounded-sm`}>
-              <h1 className={`font-bold text-3xl`}>{data.Title}</h1>
-              <div className="flex flex-row">
-              {data.Tags.map((tag,i) => {
-                  return(
-                    <div key={i} className={`justify-around badge badge-outline badge-${tag.TagColor}`}>
-                        <p className={`basis-1/2`}>
-                          {tag.tag}
-                        </p>
-                      </div>
-                    )
-                })}
+          <div key={i} className={`pt-5`}>
+            <div className={`card card-bordered	border-slate-400 shadow-xl`}>
+              <div className={`card-body rounded-sm`}>
+                <h1 className={`font-bold text-3xl`}>{data.Title}</h1>
+                  <div className="flex flex-row">
+                    {data.Tags.map((tag,i) => {
+                      return(
+                        <div key={i} className={`justify-around badge badge-outline badge-${tag.TagColor}`}>
+                          <p className={`basis-1/2`}>
+                            {tag.tag}
+                          </p>
+                        </div>
+                      )
+                  })}
+                </div>
+                <div className={``} />
+                <Link href={data.Url} className={`btn btn-success`}>内容はこちら</Link>
+                <p>最終更新日:{data.update_time.split("T")[0]}</p>
               </div>
-              <div className={``} />
-              <Link href={data.Url} className={`btn btn-success`}>内容はこちら</Link>
-              <p>最終更新日:{data.update_time.split("T")[0]}</p>
             </div>
           </div>
         )
