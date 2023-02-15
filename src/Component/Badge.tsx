55b9532b23f0fd2397a5e64022904010d918c30f
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2023-02-15 17:12:51 +0000

    todayCodingEnd...

--- src/Component/Badge.tsx
+++ src/Component/Badge.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Budge({Tag}:{Tag: string}){
     if(Tag == "途中"){
         return(
-            <div className={`justify-around badge badge-error`}>
+            <div className={`justify-around badge badge-error order-first`}>
                 <p className={`text-white`}>{Tag}</p>
             </div>
         )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
       <h1 className="hero phone:text-2xl tablet:text-3xl laptop:text-4xl desktop:text-5xl font-bold py-10">MyNotion-LearnMemo</h1>
       <div className="form-control">
         <div className="input-group">
-          <input type="text" placeholder="Tag's Search…" className="input input-bordered" />
+          <input type="text" placeholder="まだできてない～ゆるして～" className="input input-bordered" />
           <button className="btn btn-square">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
           </button>
